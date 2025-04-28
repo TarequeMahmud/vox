d8f44382300a913da159d9303e36eb654c1a7fa3
TarequeMahmud <107052189+TarequeMahmud@users.noreply.github.com>
2025-04-28 20:36:24 +0600

    fix: prevent sidebar reload on toggle
    Resolved an issue where toggling the sidebar to hide and then show would trigger a page reload. Updated the logic to ensure the sidebar state is managed without causing unnecessary reloads.

--- src/components/HomeClientLayout.tsx
+++ src/components/HomeClientLayout.tsx
@@ -14,30 +14,33 @@ const HomeClientLayout: React.FC<HomeClientLayoutProps> = ({ children }) => {
   const [showSidebar, setShowSidebar] = useState(true);
   return (
     <div className="flex flex-row justify-center h-full w-full  m-0 p-0">
-      {showSidebar && (
-        <div className="flex flex-col justify-start items-center  bg-[#8becff] h-full w-[20%] m-0 p-2  border-r-2 border-[#b3b3b3]">
-          <div className="flex flex-row justify-between h-10 w-full pt-2">
-            <Logo />
+      <div
+        className={`flex flex-col justify-start items-center  bg-[#8becff] h-full w-[20%] m-0 p-2  border-r-2 border-[#b3b3b3] ${
+          !showSidebar && "hidden"
+        }`}
+      >
+        <div className="flex flex-row justify-between h-10 w-full pt-2">
+          <Logo />
 
-            <div className="flex flex-row items-center h-7 w-14 mr-2">
-              <MdSearch
-                style={{ width: "25px", height: "25px", fill: "#0000ff" }}
-                className="cursor-pointer"
-              />
-              <PiSidebarFill
-                style={{ width: "25px", height: "25px" }}
-                className="text-[#950084] ml-2 cursor-pointer"
-                onClick={() => setShowSidebar(!showSidebar)}
-              />
-            </div>
-          </div>
-          <div className="flex flex-col justify-between w-[95%] overflow-y-hidden mt-10">
-            <NewChatButton />
-
-            <ChatlistContainer />
+          <div className="flex flex-row items-center h-7 w-14 mr-2">
+            <MdSearch
+              style={{ width: "25px", height: "25px", fill: "#0000ff" }}
+              className="cursor-pointer"
+            />
+            <PiSidebarFill
+              style={{ width: "25px", height: "25px" }}
+              className="text-[#950084] ml-2 cursor-pointer"
+              onClick={() => setShowSidebar(!showSidebar)}
+            />
           </div>
         </div>
-      )}
+        <div className="flex flex-col justify-between w-[95%] overflow-y-hidden mt-10">
+          <NewChatButton />
+
+          <ChatlistContainer />
+        </div>
+      </div>
+
       <div
         className={`flex flex-col justify-between items-center h-full ${
           showSidebar ? "w-[80%]" : "w-full"
